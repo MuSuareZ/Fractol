9b88b2e82047e6bf3a9f6c1034f31dfc1cff823d
Murillo Suarez figueiredo nunes <msuarez-@c2r2p3.hive.fi>
2020-02-06 16:54:23 +0200

    Removed get_next_line function because not needed (avoiding problems at evaluations)

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: msuarez- <msuarez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 17:37:59 by msuarez-          #+#    #+#             */
-/*   Updated: 2019/12/05 18:31:17 by msuarez-         ###   ########.fr       */
+/*   Updated: 2020/02/06 16:50:16 by msuarez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,6 @@ void			*ft_memcpy(void *dst, const void *src, size_t n);
 void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 void			*ft_memchr(const void *s, int c, size_t n);
-int				get_next_line(const int fd, char **line);
 int				ft_strequ(char const *s1, char const *s2);
 int				ft_strnequ(char const *s1, char const *s2, size_t n);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
